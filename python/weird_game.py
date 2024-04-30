a7db3de2eb5522e7af0e426730c86de62ec54d80
Mitchell Lee <mitchell@canopusnet.com>
2024-04-30 14:27:27 +1000

    sazc

--- python/weird_game.py
+++ python/weird_game.py
@@ -2,48 +2,25 @@ import copy
 import math
 import random
 
-BOARD_SIZE = 3
-BOARD_LENGTH = BOARD_SIZE * BOARD_SIZE
-
-# Board State is stored as a row major 1D array of charccaters.
-# The characters are either 'X', 'O' or ' '.
-class Board():
-  def __init__(self, action_sequence):
-    self.board = [' '] * BOARD_LENGTH
-    
-    for action in action_sequence:
-      x, y = action.position
-      self.board[x * BOARD_SIZE + y] = 'X' if action.player == 0 else 'O'
-
-  def rows(self):
-    for x in range(BOARD_SIZE):
-
+GAME_LENGTH = 4
+MAXIMUM_MOVE = 3
+WINNING_SCORE = GAME_LENGTH * MAXIMUM_MOVE - 2 # You can win by playing up if you score between the maximum possible score and 2 less than the maximum possible score
+print(WINNING_SCORE)
 
 class Action():
   def __init__(self, id):
     self.id = id
-
-  @property
-  def player(self):
-    return math.floor(self.id / BOARD_SIZE)
-  
-  @property
-  def position(self):
-    move = self.id % BOARD_SIZE
-    x = math.floor(move / BOARD_SIZE)
-    y = move % BOARD_SIZE
-    return x, y
-  
 
   def __repr__(self) -> str:
     return f"Action({self.id})"
 
 # The function will receive a list of actions to a terminal state and should return the score of the state.
 def score(action_sequence):
-  board = get_board(action_sequence)
+  score = 0
+  for action in action_sequence:
+    score += action.id
 
-  # Check Verticals
-  for x in range()
+  return 1 if score >= WINNING_SCORE else 0
 
 def upper_confidence_bound(node, exploration_exploitation_parameter):
   if node.visits == 0:
@@ -74,14 +51,12 @@ def select(node):
 
 
 total_actions = []
-for i in range(0, 2 * BOARD_SIZE):
+for i in range(1, MAXIMUM_MOVE + 1):
   total_actions.append(Action(id=i))
 
 # This function should return a list of legal actions given the current state of the game, if the game is in a terminal state, return an empty list.
 def legal_actions(action_sequence):
-  actions = []
-  for i, state in enumerate(get_board(action_sequence)):
-    if state == ' ':
-      actions.append(Action(id=i))
+  if len(action_sequence) >= GAME_LENGTH: # A legal game is 3 actions
+    return []
 
-  return actions
+  return copy.deepcopy(total_actions)
